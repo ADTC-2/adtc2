e4904df7f1fcd4e61ea4ed3e0c18d642d5664f56
ADTC2 <sectc2maranguape@gmail.com>
2022-09-03 09:15:07 -0300

    Projeto inicial do adtc2

---
+++ seguranca.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'db/config.php';
+session_start();
+
+
+if (isset($_POST['nome']) && empty($_POST['nome']) == false) {
+
+	$nome = addslashes($_POST['nome']);
+	$senha = md5(addslashes($_POST['senha']));
+    $nivel = addslashes($_POST['nivel']);
+} 	
+ 	 $sql = $pdo->query("SELECT * FROM usuarios WHERE nome='$nome' AND senha='$senha');
+
+if($sql->rowCount() > 0) {
+
+     foreach($sql->fetchAll() as $linhas){    
+ 	 
+          
+          session_regenerate_id();
+
+          $_SESSION['email']  = $linhas['nome'];
+          $_SESSION['senha']  = $linhas['senha'];
+          $_SESSION['nivel']  = $linhas['nivel'];
+
+
+          if ($_SESSION['nivel'] === "admin") {
+               header('Location:index.php');
+          }else if ($_SESSION['nivel'] === "apoio"){
+               header('Location:index_secretario.php');  
+          }else{
+               header('Location:index_tesoureiro.php');  
+          }
+
+   
+}
+           
+
+}else{
+
+        session_destroy();
+        session_unset($_SESSION['email']);
+        session_unset($_SESSION['senha']);
+        session_unset($_SESSION['nivel']);
+        header("Location:login_erro.php");         
+        exit(); 
+}
+
+
+
+?>		
